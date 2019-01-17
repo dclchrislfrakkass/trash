6a148ea1074a86acc258501cd3f43c9464c8c0c0
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-01-17 22:59:47 +0100

    requete in var try

--- home_pdo_2/index.php
+++ home_pdo_2/index.php
@@ -1,17 +1,7 @@
 <?php
 require 'pdo.php';
+// require 'requete.php';
+require 'test.php';
 
 
-try{
-    $requete = $bd->query("SELECT * FROM cas WHERE NumEtude = 2012");
-    $resultats = $requete->fetchAll();
-}
-catch(Exception $e)
-{
-    exit('<b>Catched exception at line '. $e->getLine() .' (code : '. $e->getCode() .') :</b> '. $e->getMessage());
-}
-foreach($resultats as $resultat) {
-    var_dump($resultat);
-    echo '<br />';
-    echo '<br />';
-}
+var_dump($stmt);
---
+++ home_pdo_2/test.php
@@ -0,0 +1,21 @@
+
+
+
+<?php
+
+$motCle = 1978;
+
+$stmt= $bd->prepare("SELECT * FROM cas WHERE NumEtude= $motCle");
+$stmt->execute(array($motCle));
+
+$resultats = $stmt->fetchAll();
+
+foreach($resultats as $resultat) {
+    var_dump('<b>Nom du cas : </b>'.$resultat['NomEtude'].'<b> Observation : </b>'.$resultat['ResumeWeb']);
+    echo '<br />';
+    echo '<br />';
+}
+
+// $stmt = $requete->fetchALl(){
+//     $motCle = $row['']
+// }
